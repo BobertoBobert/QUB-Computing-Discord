e781bd2e07d7585071dd751bb3465bcf7d876042
Christopher Magee <christophermagee10@gmail.com>
2020-10-05 22:58:59 +0100

    Swapped switch for if statement

--- index.js
+++ index.js
@@ -14,52 +14,65 @@ client.on('ready', () => {
 
 // Create an event listener for messages
 client.on('message', message => {
+switch
+    if (message.author.bot) return;
+
     // If the message is "ping"
-    if (message.content.includes(" addNote ")) {
-        let targetChannel = client.channels.cache.get('755400893175955548');
+    if (message.content.includes("?addNote")) {
+        console.log('test');
+        let targetChannelID = channelFinder(message.channel.id);
 
         let noCommandVersion = message.content;
         noCommandVersion = ((noCommandVersion.substring(0, noCommandVersion.indexOf("addNote"))) +
-            noCommandVersion.substring(noCommandVersion.indexOf("addNote") + 7, noCommandVersion.length - 1));
+            noCommandVersion.substring(noCommandVersion.indexOf("addNote") + 7, noCommandVersion.length));
+
+        let targetChannel = client.channels.cache.get(targetChannelID)
         targetChannel.send(noCommandVersion);
+    }
+
+    if (message.content.includes("ping")) {
+        message.channel.send('pong');
     }
 });
 
 
 
 client.on('messageReactionAdd', (messageReaction, user) => {
-    let targetChannel = '';
-    switch (messageReaction.message.channel.id) {
-        case '761548502051586068': //networks
-            targetChannel = '757969407199674422';
-            break;
-        case '757969503148834826': //1027 Programming
-            targetChannel = '760850283172462672';
-            break;
-        case '757969536317390960': //procedural Programming
-            targetChannel = '760561671164526612';
-            break;
-        case '760520891972714518': //Maths
-            targetChannel = '757969446395707432';
-            break;
 
-        default:
-            console.log('nope');
-            return;
-    }
+
     if (messageReaction.emoji.name == '🗒️') {
-        let notesChannel = messageReaction.message.client.channels.cache.get(targetChannel);
+        let targetChannelID = channelFinder(messageReaction.message.channel.id);
+        console.log('Correct reaction');
+        console.log(targetChannelID);
+        let targetChannel = client.channels.cache.get(targetChannelID);
 
-        let noCommandVersion = messageReaction.message.content;
-        noCommandVersion = ((noCommandVersion.substring(0, noCommandVersion.indexOf("addNote"))) +
-            noCommandVersion.substring(noCommandVersion.indexOf("addNote") + 7, noCommandVersion.length - 1));
-        targetChannel.send(noCommandVersion);
+        targetChannel.send(messageReaction.message.content);
 
 
     }
 
 
 })
+
+
+
+function channelFinder(id) {
+    if (id == '761548502051586068') { //Networking
+        return ('757969407199674422');
+    }
+    if (id == '760850283172462672') { //Programming
+        return ('757969503148834826');
+    }
+    if (id == '760561671164526612') { // Procedural Programming
+        return ('757969536317390960');
+    }
+    if (id == '757969446395707432') { // Maths Discussion
+        return ('760520891972714518');
+    }
+    if (id == '762784686560378927') { //Bot Testing
+        return ('762785719026057246');
+    }
+}
 
 
 
