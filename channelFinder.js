74a0c0b0e9872e25214ec584630160bd6ed2f658
Christopher <christophermagee10@gmail.com>
2020-10-06 18:39:32 +0100

    Changed channel finder to work with array

--- channelFinder.js
+++ channelFinder.js
@@ -1,23 +1,17 @@
+const fs = require('fs');
+
 module.exports = {
     execute(id) {
-        if (id == '761548502051586068') { //Networking
-            return ('757969407199674422');
-        }
-        if (id == '760850283172462672') { //Programming
-            return ('757969503148834826');
-        }
-        if (id == '760561671164526612') { // Procedural Programming
-            return ('757969536317390960');
-        }
-        if (id == '757969446395707432') { // Maths Discussion
-            return ('760520891972714518');
-        }
-        if (id == '762784686560378927') { //Bot Testing
-            return ('762785719026057246');
+        console.log(id);
+
+        let rawData = fs.readFileSync('./channels.json');
+        let channelArray = JSON.parse(rawData).channels;
+        for (const channels of channelArray) {
+            if (id == channels[0]) {
+                return channels[1];
+            }
         }
 
-        if (id == '755400862502748173') { //general to channel 2 in BT server TESTING
-            return ('755400893175955548');
-        }
+
     }
 }
