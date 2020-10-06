cb988ef0bda955dbd78483077c2043f925dc444e
Christopher <christophermagee10@gmail.com>
2020-10-06 18:53:22 +0100

    Added comments and fixed formatting

--- channelFinder.js
+++ channelFinder.js
@@ -2,12 +2,15 @@ const fs = require('fs');
 
 module.exports = {
     execute(id) {
-        console.log(id);
-
+        //First we read the json file
         let rawData = fs.readFileSync('./channels.json');
+        //then we save the array in it as a variable
         let channelArray = JSON.parse(rawData).channels;
+        //iterate through it
         for (const channels of channelArray) {
+            //checking if our ID is on the list
             if (id == channels[0]) {
+                console.log(`Original channel was ${channels[2]}`)
                 return channels[1];
             }
         }
