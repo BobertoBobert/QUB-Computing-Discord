2b2399455faf0bc45f1a40c7489a06060882f35b
jhbuchanan45 <jbuchanan21@qub.ac.uk>
2020-10-07 09:52:45 +0100

    Rewrite exec functionality, disable node install temporarily

--- commands/update.js
+++ commands/update.js
@@ -1,52 +1,48 @@
-const { execSync } = require('child_process').execSync;
+const child_process = require('child_process');
 
 module.exports = {
 	name: 'update',
 	description: 'Runs one or both of 2 bash scripts, either just fetching the master repo branch or also updating node modules',
 	execute(message, args, client) {
-		const node 
-		if (args[0]=="fetch" || args[0] == null) {
+		if (args[0]=="fetch" || args[0]==null) {
 			message.channel.send("Running git fetch on bot!")
 			
 			// run git fetch script
-			const fetchScript = execSync('sh gitfetch.sh /bot-files');
-			fetchScript.stdout.on('data', (data)=>{
-				message.channel.send(data);				
+			child_process.exec('sh gitfetch.sh', (error, stdout) => {
+				if (error) {
+					console.error(`Exec error: ${error}`)
+				}
+				console.log(`stdout: ${stdout}`);
+				message.channel.send("Success! Restarting...").then(() => client.destroy()).then((client) => client.login(client.config.token));
 			})
-			fetchScript.stderr.on('data', (data)=>{
-				message.channel.send(data);
-			})
+		} else if (args[0]=="node") {
 
-			message.channel.send("Restarting")
+			message.channel.send("Node install request detected!")
+			// message.channel.send("Running git fetch before updating node modules");
 
-			client.destroy().then(client.login(client.config.token))
-		} 
-		else if (args[0]=="node") {
-			message.channel.send("Running git fetch before updating node modules")
+			// // run git fetch script
+			// const fetchScript = execSync('sh gitfetch.sh /bot-files');
+			// fetchScript.stdout.on('data', (data)=>{
+			// 	message.channel.send(data);				
+			// })
+			// fetchScript.stderr.on('data', (data)=>{
+			// 	message.channel.send(data);				
+			// })
 
-			// run git fetch script
-			const fetchScript = execSync('sh gitfetch.sh /bot-files');
-			fetchScript.stdout.on('data', (data)=>{
-				message.channel.send(data);				
-			})
-			fetchScript.stderr.on('data', (data)=>{
-				message.channel.send(data);				
-			})
+			// message.channel.send("Updating node modules")
 
-			message.channel.send("Updating node modules")
+			// // run node update script
+			// const modulesscript = execSync('sh nodeinstall.sh /bot-files');
+			// modulesscript.stdout.on('data', (data)=>{
+			// 	message.channel.send(data);				
+			// })
+			// modulesscript.stderr.on('data', (data)=>{
+			// 	message.channel.send(data);	
+			// })
 
-			// run node update script
-			const modulesscript = execSync('sh nodeinstall.sh /bot-files');
-			modulesscript.stdout.on('data', (data)=>{
-				message.channel.send(data);				
-			})
-			modulesscript.stderr.on('data', (data)=>{
-				message.channel.send(data);	
-			})
+			// message.channel.send("Stopping node!")
 
-			message.channel.send("Stopping node!")
-
-			client.destroy().then(process.exit());
+			// client.destroy().then(() => process.exit());
 
 		} else {
 			message.channel.send("Incorrect arguments!")
