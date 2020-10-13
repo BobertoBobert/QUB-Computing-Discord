16f3ee7574fb29cb60098bd7bb389941a2c68333
Christopher <christophermagee10@gmail.com>
2020-10-13 10:57:15 +0100

    Added permissions

--- index.js
+++ index.js
@@ -37,12 +37,15 @@ client.on('ready', () => {
 //when a message is recieved, this will run
 client.on('message', message => {
 
-    //checks if the message was sent by a bot or does not contain a prefix. In either of these cases it will then return without doing anything. This will be what happens for most messages
+    //checks if the message was sent by a bot or does not contain a prefix. 
+    //In either of these cases it will then return without doing anything. 
+    //This will be what happens for most messages
     if (!message.content.startsWith(config.prefix) || message.author.bot) {
         return;
     }
 
-    // This piece of string manipulation removes the prefix and whitespace, and then splits the command into an array of arguments by spaces
+    // This piece of string manipulation removes the prefix and whitespace,
+    // and then splits the command into an array of arguments by spaces
     const args = message.content.slice(config.prefix.length).trim().split(' ');
     // This removes the command name from the array, and sets it as its own variable
     const commandName = args.shift().toLowerCase();
@@ -54,9 +57,31 @@ client.on('message', message => {
     }
 
 
+    //store the command as an object
+    const command = client.commands.get(commandName);
+
+
+
+    //Check if a certain role is needed to run the command
+    //if yes, then ensure the user has the role
+    if (!(command.roles === undefined || message.member.roles.cache.some(role => {
+        let isAllowed = false;
+        for (i = 0; i < command.roles.length; i++) {
+            console.log(command.roles[i] + role.name)
+            if (command.roles[i] === role.name) {
+                isAllowed = true;
+            }
+        }
+        return isAllowed;
+    }))) {
+        message.reply('Sorry, you do not have permission to use that command');
+        return;
+    }
+
+
     //Execute the command, and tell the user if it fails
     try {
-        client.commands.get(commandName).execute(message, args, client);
+        command.execute(message, args, client);
     } catch (error) {
         console.error(error);
         message.reply('there was an error trying to execute that command!');
@@ -68,17 +93,17 @@ client.on('message', message => {
 // When a message is reacted to, check that if reaction is the notebook emoji
 client.on('messageReactionAdd', (messageReaction, user) => {
     if (messageReaction.emoji.name == '🗒️') {
-            // Before do anything, verify that there have been at least 3 reactions to the message
-            // and that it has not already been sent into the notes channel. We will react to 
-            // messages that have been processed  with our own reaction to mark them as having 
-            // been processed. ALso makes sure message was not moved with command
-            if (messageReaction.count <3 || messageReaction.me || messageReaction.message.content.toLowerCase().startsWith(`${config.prefix}addnote`)){
-                return;
-            }
-            messageReaction.message.react('🗒️');
+        // Before do anything, verify that there have been at least 3 reactions to the message
+        // and that it has not already been sent into the notes channel. We will react to 
+        // messages that have been processed  with our own reaction to mark them as having 
+        // been processed. ALso makes sure message was not moved with command
+        if (messageReaction.count < 3 || messageReaction.me || messageReaction.message.content.toLowerCase().startsWith(`${config.prefix}addnote`)) {
+            return;
+        }
+        messageReaction.message.react('🗒️');
 
 
-            //check the target id using the id of the current channel
+        //check the target id using the id of the current channel
 
         let targetChannelID = channelFinder.execute(messageReaction.message.channel.id);
 
@@ -90,7 +115,7 @@ client.on('messageReactionAdd', (messageReaction, user) => {
             let credit = `Notes provided by ${messageReaction.message.author}\n\n`;
             targetChannel.send(credit + messageReaction.message.content);
 
-        } catch(error){
+        } catch (error) {
             console.error(error);
         }
     }
