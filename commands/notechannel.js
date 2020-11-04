3a8eb76ed2a08a6ad806ef2d61d94b7fc306ab3e
Christopher Magee <43120590+BobertoBobert@users.noreply.github.com>
2020-11-04 13:14:52 +0000

    Allows channels to be in more than 1 notes link (#24)
    But prevents exact duplicates

--- commands/notechannel.js
+++ commands/notechannel.js
@@ -16,15 +16,14 @@ module.exports = {
         let rawData = fs.readFileSync('./channels.json');
         let channelArray = JSON.parse(rawData).channels;
 
-        //This ensures there is no overlap between the different notes/discussion channel links
+        //This ensures that each link is unique
         for (const existingNote of channelArray) {
-            for (i = 0; i < 2; i++) {
-                if (existingNote[i] == newLink[i]) {
-                    message.reply('Sorry, that channel is already used in a link');
-                    return;
+            if (existingNote[0] == newLink[0] && existingNote[1] == newLink[1]) {
+                message.reply('Sorry, that link already exists');
+                return;
 
-                }
             }
+
             if (existingNote[2] == newLink[2]) {
                 message.reply('Sorry, that description is already used for a link');
                 return;
